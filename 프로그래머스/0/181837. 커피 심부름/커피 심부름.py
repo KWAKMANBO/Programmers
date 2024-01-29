ccd3dfc82676a3a5f072afad215144620d07ea56
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2024-01-29 20:43:55 +0900

    [level 0] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,12 @@
+def solution(order):
+    answer = 0
+    for i in order:
+        if "cafelatte" in i:
+            answer += 5000
+        elif "americano" in  i:
+            answer += 4500
+        else:
+            answer += 4500
+        
+    
+    return answer
