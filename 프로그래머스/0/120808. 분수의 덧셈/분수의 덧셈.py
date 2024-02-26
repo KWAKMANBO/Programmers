f9355c888f5ce22239af2edbe0cbd10cf302ea89
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2024-02-26 10:45:12 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,11 +1,15 @@
-import math
-
 def solution(numer1, denom1, numer2, denom2):
-    answer = []
-    a = denom1 * denom2
-    b = denom1*numer2 + denom2*numer1
-    c = math.gcd(a,b)
-    answer.append(b//c)
-    answer.append(a//c)
+    numer = numer1*denom2 + numer2*denom1
+    denom = denom1*denom2
+    tmp = gcd(numer,denom)
+    answer = [numer//tmp, denom//tmp]
     return answer
+
+def gcd(m,n):
+    if m < n :
+        m , n = n, m
     
+    if m % n == 0 : 
+        return n
+    else:
+        return gcd(n, m%n)
