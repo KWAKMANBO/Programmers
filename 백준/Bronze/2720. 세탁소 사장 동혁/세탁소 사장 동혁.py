6941ee8873d3938595ed7c4109335248b8bd2811
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2024-05-31 13:52:56 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -3,6 +3,7 @@ import sys
 n = int(sys.stdin.readline())
 
 coins = [25, 10, 5, 1]
+answer = []
 
 for _ in range(n):
     m = int(sys.stdin.readline().rstrip())
@@ -10,4 +11,7 @@ for _ in range(n):
     for i in coins:
         tmp.append(m // i)
         m %= i
-    print(*tmp)
+    answer.append(tmp)
+
+for i in answer:
+    print(*i)
