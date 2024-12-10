57e5317ee95f89a9d9d973bed1857df258c0bbde
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2024-12-10 10:48:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    dict = {"zero": '0', "one": '1', "two": '2', "three": '3', "four": '4', "five": '5', "six": '6', "seven": '7',
+            "eight": '8',
+            "nine": '9'}
+    answer = 0
+    for n in list(dict.keys()):
+        if n in s:
+            s = s.replace(n, dict[n])
+    return int(s)
