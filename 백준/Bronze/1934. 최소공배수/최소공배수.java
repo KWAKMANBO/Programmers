78966ffa0d073a4e0b3ee0a326d04712ae723760
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-02-15 03:03:19 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(lcm(a, b)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static int gcd(int a, int b) {
+        if (a < b) {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        while (b != 0) {
+            int tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        return a;
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
