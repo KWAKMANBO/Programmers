48cd2af484b7b01b819e294f89d7a1a6237da2de
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-02-18 02:01:38 +0900

    [level 2] Title: 귤 고르기, Time: 39.00 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,14 @@
+from collections import Counter
+
 def solution(k, tangerine):
     answer = 0
-    tangerineCnt = {}
-    for t in tangerine:
-        if t not in tangerineCnt:
-            tangerineCnt[t] = 1
-        else:
-            tangerineCnt[t] += 1
+    cnt =0
 
-    lst = list(tangerineCnt.values())
-    lst.sort(reverse=True)
-    cnt = 0
-    for i in range(len(lst)):
-        cnt += lst[i]
+    cntLst = Counter(tangerine)
+    cntLst = cntLst.most_common()
+
+    for c in cntLst:
+        cnt += c[1]
         answer +=1
-        if cnt >= k:
+        if cnt >= k :
             return answer
