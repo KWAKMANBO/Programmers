f004da11a64c08db6ed37e7f3a84358b0c8c0eef
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-03-13 17:29:05 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 740 ms, Memory: 80840 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,25 +1,29 @@
 import sys
 
+
 def get_primes(n):
-    end = int(n**0.5)+1
-    arr =  [i for i in range(n+1)]
-    for i in range(2,end):
+    end = int(n ** 0.5) + 1
+    arr = [i for i in range(n + 1)]
+    for i in range(2, end):
         if arr[i] == 0:
             continue
-        for j in range(i*i,n+1, i):
+        for j in range(i * i, n + 1, i):
             arr[j] = 0
     arr[0] = 0
     arr[1] = 0
-    return arr
+    return arr, [i for i in arr[2:] if i]
+
 
 input = sys.stdin.readline
 
 n = int(input())
-primes = get_primes(1000000)
+arr, primes = get_primes(1000000)
+
 for _ in range(n):
     num = int(input())
-    cnt =0
-    for j in range(num//2 + 1):
-        if primes[j] and primes[num-j]:
-            cnt +=1
+    cnt = 0
+    for p in primes:
+        if p > num // 2: break
+        if arr[num - p]:
+            cnt += 1
     print(cnt)
