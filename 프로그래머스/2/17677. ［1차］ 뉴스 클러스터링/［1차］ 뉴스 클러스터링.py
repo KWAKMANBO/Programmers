9afd4b3e9171009c223b5e5e9fde6112287f1688
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-04-04 16:20:13 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 8.26 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,54 +1,17 @@
-# 합집합
-def union(arr1, arr2):
-    answer = []
-    cnt = {}
-
-    for i in arr1:
-        if i in cnt.keys():
-            continue
-
-        if i in arr2:
-            cnt[i] = max(arr1.count(i), arr2.count(i))
-        else:
-            cnt[i] = arr1.count(i)
-
-    for i in arr2:
-        if i not in cnt.keys():
-            cnt[i] = arr2.count(i)
-    for c in cnt:
-        answer.extend([c] * cnt[c])
-    return answer
-
-
-# 교집합
-def intersection(arr1, arr2):
-    cnt = {i: min(arr1.count(i), (arr2.count(i))) if i in arr2 else 0 for i in arr1}
-    answer = []
-    for c in cnt:
-        answer.extend([c] * cnt[c])
-
-    return answer
-
-
 def solution(str1, str2):
     answer = 0
     str1 = str1.lower()
     str2 = str2.lower()
-    set1 = []
-    set2 = []
+    set1 = [str1[i:i+2] for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    set2 = [str2[i:i+2] for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
 
-    for i in range(0, len(str1) - 1):
-        if str1[i:i + 2].isalpha():
-            set1.append(str1[i:i + 2])
+    union_set = set(set1) | set(set2)
+    intersection_set = set(set1) & set(set2)
 
-    for i in range(0, len(str2) - 1):
-        if str2[i:i + 2].isalpha():
-            set2.append(str2[i:i + 2])
+    multi_union = sum(max(set1.count(u), set2.count(u)) for u in union_set)
+    multi_intersection = sum(min(set1.count(i),set2.count(i)) for i in intersection_set)
 
-    uni = len(union(set1, set2))
-    inter = len(intersection(set1, set2))
-
-    if uni == 0:
+    if multi_union == 0 and multi_union == 0:
         return 65536
     else:
-        return int((inter / uni) * 65536)
+        return int((multi_intersection/multi_union)*65536 )
