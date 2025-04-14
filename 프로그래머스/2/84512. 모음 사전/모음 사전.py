2fd9da24c9193c48b19dedbb979d51651bb84736
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-04-15 03:19:19 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,5 @@
-from itertools import product
-
-
 def solution(word):
-    chrs = ['A', 'E', 'I', 'O', 'U']
-    word = tuple(word)
-    answer = []
-    for i in range(5):
-        for w in product(chrs, repeat=i + 1):
-            answer.append(w)
-    answer.sort()
-    return answer.index(word) + 1
+    answer = 0
+    for i, n in enumerate(word):
+        answer += (5 ** (5 - i) // 4) * "AEIOU".index(word[i]) + 1
+    return answer
