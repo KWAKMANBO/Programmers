7d972c731f176795369d7f85af77464f4a410986
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-04-22 01:38:30 +0900

    [level 2] Title: 택배상자, Time: 316.79 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,26 +1,14 @@
-from collections import deque
-
-
 def solution(order):
-    answer = 0
-    tmp_belt = deque([])
-    belt = deque([i for i in range(1, len(order) + 1)])
+    stack = []
+    N = len(order)
+    i = 1
     idx = 0
-    while True:
-        if belt and belt[0] == order[idx]:
-            belt.popleft()
-            answer += 1
+    while i < N + 1:
+        stack.append(i)
+        while stack[-1] == order[idx]:
             idx += 1
-        elif tmp_belt and tmp_belt[-1] == order[idx]:
-            tmp_belt.pop()
-            answer += 1
-            idx += 1
-        elif belt and belt[0] != order[idx]:
-            tmp_belt.append(belt.popleft())
-        elif not belt and not tmp_belt:
-            break
-        else :
-            break
-
-
-    return answer
+            stack.pop()
+            if len(stack) == 0:
+                break
+        i += 1
+    return idx
