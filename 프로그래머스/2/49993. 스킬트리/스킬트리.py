b7fbe5ff554d97755d751a48fa7fba2749ac6bd9
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-04-23 14:16:57 +0900

    [level 2] Title: 스킬트리, Time: 0.03 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,12 +1,20 @@
+from collections import deque
+
+
 def solution(skill, skill_trees):
     answer = 0
-    for st in skill_trees:
-        tmp = ""
-        for w in st:
-            if w in skill:
-                tmp += w
-        if not tmp or (tmp and tmp[0] == skill[0] and tmp in skill):
-            answer += 1
 
+    for st in skill_trees:
+        q = deque(skill)
+        flag = True
+        for w in st:
+            if w in q and w == q[0]:
+                q.popleft()
+            elif w in q and w != q[0]:
+                flag = False
+                break
+
+        if flag:
+            answer += 1
 
     return answer
