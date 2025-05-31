86b017ac2fbf24c02b0f0464418da271bcf74cdd
KWAKMANBO <kwak98hs@gmail.com>
2025-06-01 02:43:21 +0900

    25/05/31:(Sort) 삽입 정렬 Java 버전 추가

---
+++ 이코테 학습/4.Sort/Sort_Java/InsertionSortMyvVersion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class InsertionSortMyvVersion {
+    public static void main(String[] args) {
+        int[] array = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    int tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
