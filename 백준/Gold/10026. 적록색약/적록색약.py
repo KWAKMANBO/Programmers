b94134413b4182029c2a83d3966832235db5fb98
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-06-16 16:14:12 +0900

    [Gold V] Title: 적록색약, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,7 +1,5 @@
 import sys
 
-from collections import deque
-
 input = sys.stdin.readline
 
 N = int(input())
@@ -13,47 +11,25 @@ dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def bfs1(start_node, val):
-    queue = deque()
-    queue.append(start_node)
-    visited[start_node[0]][start_node[1]] = 1
+def dfs1(start_node, val):
+    x, y = start_node
+    visited[x][y] = 1
+    stack = [start_node]
 
-    while queue:
-        x, y = queue.popleft()
+    while stack:
+        x, y = stack.pop()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if visited[nx][ny] == 0 and board[nx][ny] == val:
-                    visited[nx][ny] = 1
-                    queue.append((nx, ny))
-
-
-# 빨강이랑 초록을 똑같이 판단
-def bfs2( start_node, val):
-    if val == 'G':
-        val = 'R'
-
-    queue = deque()
-    queue.append(start_node)
-    visited[start_node[0]][start_node[1]] = 1
-
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if visited[nx][ny] == 0 and board[nx][ny] == val:
-                    visited[nx][ny] = 1
-                    queue.append((nx, ny))
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0 and board[nx][ny] == val:
+                stack.append((nx, ny))
+                visited[nx][ny] = 1
 
 
 area1 = []
 for i in range(N):
     for j in range(N):
         if visited[i][j] == 0:
-            bfs1((i, j), board[i][j])
+            dfs1((i, j), board[i][j])
             area1.append(board[i][j])
 
 area2 = []
@@ -68,13 +44,14 @@ for i in range(N):
             new_board[i].append('R')
         else:
             new_board[i].append('B')
-# 적록생명 영역 찾기 
+# 적록생명 영역 찾기
 board = new_board
 for i in range(N):
     for j in range(N):
         if visited[i][j] == 0:
-            bfs2((i, j), board[i][j])
+            val = 'R' if board[i][j] != 'B' else 'B'
+            dfs1((i, j), board[i][j])
             area2.append(board[i][j])
-            
+
 print(len(area1))
 print(len(area2))
