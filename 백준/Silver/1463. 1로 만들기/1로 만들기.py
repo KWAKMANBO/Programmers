6d8731e6eb4abe68a328fce92d320ba5e930cbbe
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2025-07-06 23:16:38 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,19 @@
-import sys
+x = int(input())
+dp = {1: 0}
 
-input = sys.stdin.readline
 
-N = int(input())
+def rec(n):
+    if n in dp.keys():
+        return dp[n]
+    if (n % 3 == 0) and (n % 2 == 0):
+        dp[n] = min(rec(n // 3) + 1, rec(n // 2) + 1)
+    elif n % 3 == 0:
+        dp[n] = min(rec(n // 3) + 1, rec(n - 1) + 1)
+    elif n % 2 == 0:
+        dp[n] = min(rec(n // 2) + 1, rec(n - 1) + 1)
+    else:
+        dp[n] = rec(n - 1) + 1
+    return dp[n]
 
-dp = [0] * (N + 1)
 
-for i in range(2, N + 1):
-    dp[i] = dp[i - 1] + 1
-
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
-
-print(dp[N])
+print(rec(x))
