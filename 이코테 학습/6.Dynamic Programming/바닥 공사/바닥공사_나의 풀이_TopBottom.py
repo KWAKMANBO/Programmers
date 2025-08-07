477073e75317297035622e4d64e67de3a4480870
KWAKMANBO <kwak98hs@gmail.com>
2025-08-07 19:59:44 +0900

    (Dynamic Programming) - 바닥 공사 나의 풀이 TopDown

---
+++ 이코테 학습/6.Dynamic Programming/바닥 공사/바닥공사_나의 풀이_TopBottom.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+blocks = [0] * 1001
+dp = [0] * 1001
+dp[1] = 1
+dp[2] = 3
+
+
+def solve(n):
+    if dp[n] != 0:
+        return dp[n]
+
+    dp[n] = solve(n - 2) * 2 + solve(n - 1)
+    return dp[n]
+
+
+print(solve(N))
