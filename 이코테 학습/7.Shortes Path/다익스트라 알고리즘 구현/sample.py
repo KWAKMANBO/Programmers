a6713f02a22cecee801480b0dbb77093582ce6c5
KWAKMANBO <kwak98hs@gmail.com>
2025-08-09 13:03:14 +0900

    (Shortest Path)다익스트라 알고리즘 간단한 버전 학습중

---
+++ 이코테 학습/7.Shortes Path/다익스트라 알고리즘 구현/sample.py
@@ -0,0 +1,71 @@
+# Input
+# 6 11
+# 1
+# 1 2 2
+# 1 3 5
+# 1 4 1
+# 2 3 3
+# 2 4 2
+# 3 2 3
+# 3 6 5
+# 4 3 3
+# 4 5 1
+# 5 3 1
+# 5 6 2
+
+import sys
+
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+
+start = int(input())
+
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    # graph[a] = [(b,c)]는
+    # a에서 b로 갈때 c의 비용이 드는 것을 의미함
+    graph[a].append((b, c))
+
+
+# 비용이 제일 적게드는 노드 탐색하기
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+
+    for i in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INF")
+    else:
+        print(distance[i])
