8c0bf4f9e541d02c14525670daa27a215ee86a87
KWAKMANBO <kwak98hs@gmail.com>
2025-08-11 15:17:36 +0900

    (Shortest Path)다익스트라 알고리즘 간단한 버전 답안 코드

--- 이코테 학습/7.Shortes Path/다익스트라 알고리즘 구현/간단한 다익스트라 알고리즘_답안코드.py
+++ 이코테 학습/7.Shortes Path/다익스트라 알고리즘 구현/간단한 다익스트라 알고리즘_답안코드.py
@@ -16,7 +16,6 @@
 import sys
 
 input = sys.stdin.readline
-
 INF = int(1e9)
 
 n, m = map(int, input().split())
@@ -25,44 +24,45 @@ start = int(input())
 
 graph = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
-
 distance = [INF] * (n + 1)
 
 for _ in range(m):
+    # a는 시작점
+    # b는 종점
+    # c는 a에서 b로 가는데 드는 비용
     a, b, c = map(int, input().split())
-    # graph[a] = [(b,c)]는
-    # a에서 b로 갈때 c의 비용이 드는 것을 의미함
     graph[a].append((b, c))
 
 
-# 비용이 제일 적게드는 노드 탐색하기
-def get_smallest_node():
+def get_smallest():
     min_value = INF
-    index = 0
+    idx = 0
     for i in range(1, n + 1):
         if distance[i] < min_value and not visited[i]:
             min_value = distance[i]
-            index = i
-    return index
+            idx = i
+    return idx
 
 
-def dijkstra(start):
+def dijkstar(start):
     distance[start] = 0
     visited[start] = True
 
-    for j in graph[start]:
-        distance[j[0]] = j[1]
+    for i in range(1, n + 1):
+        for j in graph[start]:
+            distance[j[0]] = j[1]
 
+    # n-1번 반복해서 각 노드별 최단 거리 찾기
     for i in range(n - 1):
-        now = get_smallest_node()
-        visited[now] = True
-        for j in graph[now]:
-            cost = distance[now] + j[1]
+        smallest = get_smallest()
+        visited[smallest] = True
+        for j in graph[smallest]:
+            cost = distance[smallest] + j[1]
             if cost < distance[j[0]]:
                 distance[j[0]] = cost
 
 
-dijkstra(start)
+dijkstar(start)
 
 for i in range(1, n + 1):
     if distance[i] == INF:
