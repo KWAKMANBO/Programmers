be8c26b021c1ba4c2304d7e6f71dd5495271f182
KWAKMANBO <kwak98hs@gmail.com>
2025-08-13 22:22:28 +0900

    (Shortest Path) 플로이드 워셜 알고리즘 나의 풀이

---
+++ 이코테 학습/7.Shortes Path/플로이드 워셜 알고리즘/플로이드 워셜 알고리즘_나의 풀이.py
@@ -0,0 +1,39 @@
+# Input
+# 4
+# 7
+# 1 2 4
+# 1 4 6
+# 2 1 3
+# 2 3 7
+# 3 1 5
+# 3 4 4
+# 4 3 2
+
+import sys
+
+input = sys.stdin.readline
+
+# 노드 개수
+n = int(input())
+# 간선 개수
+m = int(input())
+INF = int(1e9)
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(n + 1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    s, e, v = map(int, input().split())
+    graph[s][e] = v
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        for k in range(1,n+1):
+            graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
+
+for i in range(1, n+1):
+    print(*graph[i][1:])
+
+
