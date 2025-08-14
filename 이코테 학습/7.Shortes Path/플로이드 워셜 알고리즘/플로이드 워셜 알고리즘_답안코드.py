dd38577dadfc32ddee68c75f7d4713f9a2201910
KWAKMANBO <kwak98hs@gmail.com>
2025-08-14 17:18:36 +0900

    (Shortest Path)플로이드 워셜 알고리즘 답안코드

--- 이코테 학습/7.Shortes Path/플로이드 워셜 알고리즘/플로이드 워셜 알고리즘_답안코드.py
+++ 이코테 학습/7.Shortes Path/플로이드 워셜 알고리즘/플로이드 워셜 알고리즘_답안코드.py
@@ -1,3 +1,13 @@
+# Input
+# 4
+# 7
+# 1 2 4
+# 1 4 6
+# 2 1 3
+# 2 3 7
+# 3 1 5
+# 3 4 4
+# 4 3 2
 import sys
 
 input = sys.stdin.readline
@@ -21,4 +31,13 @@ for _ in range(m):
 for k in range(1,n+1):
     for a in range(1,n+1):
         for b in range(1,n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] )
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print("INFINITY", end = " ")
+        else:
+            print(graph[a][b], end = " ")
+
+    print()
