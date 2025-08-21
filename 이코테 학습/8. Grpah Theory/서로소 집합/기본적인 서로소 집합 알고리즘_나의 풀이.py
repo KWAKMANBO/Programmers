8948922410f346ae17bde5b31d3b03d7a9fcdcd4
KWAKMANBO <kwak98hs@gmail.com>
2025-08-22 02:10:41 +0900

    (Graph Theory )서로소알고리즘 나의 풀이

--- 이코테 학습/8. Grpah Theory/서로소 집합/기본적인 서로소 집합 알고리즘_나의 풀이.py
+++ 이코테 학습/8. Grpah Theory/서로소 집합/기본적인 서로소 집합 알고리즘_나의 풀이.py
@@ -1,3 +1,10 @@
+# Input
+# 6 4
+# 1 4
+# 2 3
+# 2 4
+# 5 6
+
 import sys
 
 input = sys.stdin.readline
@@ -5,11 +12,40 @@ input = sys.stdin.readline
 # 노드와 간선개수 입력
 n, m = map(int, input().split())
 
-arr = [0] * (n + 1)
+arr = [i for i in range(n + 1)]
 
 
+# 부모를 찾는 함수
+def find(x):
+    # 부모가 0 => 최상위 노드
+    if arr[x] == x:
+        return x
+    else:
+        return find(arr[x])
 
-def find(arr,x):
-    
 
-def union():
+def union(x, y):
+    parent_x = find(x)
+    parent_y = find(y)
+
+    # y의 부모가 더 작더라면 y를 부모로 만들기
+    if parent_y < parent_x:
+        arr[parent_x] = parent_y
+    else:
+        arr[parent_y] = parent_x
+
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    union(s, e)
+
+# 원소가 속한 집합의 부모를 출력하기
+roots = [find(i) for i in range(1, n + 1)]
+print(f"각 원소가 속한 집합 :", end=" ")
+for r in roots:
+    print(r, end=" ")
+print()
+# 각 원소의 부모 출력
+print(f"부모 테이블 : ", end=" ")
+for a in arr[1:]:
+    print(a, end=" ")
