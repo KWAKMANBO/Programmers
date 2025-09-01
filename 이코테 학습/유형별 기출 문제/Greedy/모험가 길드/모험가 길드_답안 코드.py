cd458c2f3c1b150499f71ee93099a9d96d94dbda
KWAKMANBO <kwak98hs@gmail.com>
2025-09-01 15:47:11 +0900

    유형별 기출 문제(그리디) 모험가 길드 답안 코드

---
+++ 이코테 학습/유형별 기출 문제/Greedy/모험가 길드/모험가 길드_답안 코드.py
@@ -0,0 +1,19 @@
+# Input
+# 5
+# 2 3 1 2 2
+
+n = int(input())
+data = list(map(int, input().split()))
+
+data.sort()
+
+result = 0
+count = 0
+
+for i in data:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0
+
+print(result)
