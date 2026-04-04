8417104280d9ff6f8e648fecf9392afddf12a610
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-04-04 18:32:02 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 5.71 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -1,25 +1,48 @@
 def solution(message, spoiler_ranges):
-    spoiler_words = []
+    answer = 0
+    no_spoiler_ranges = []
     no_spoiler_words = []
+    spoiler_words = []
+    # no_spoiler_ranges를 담을 떄 사용할 idx
     idx = 0
-    for w in message.split():
-        s = message.index(w, idx)
-        e = s + len(w) - 1
-        idx = e + 1
 
-        is_spoiler = False
-        for i in spoiler_ranges:
-            ss, ee = i[0], i[1]
-            if s <= ee and e >= ss:
-                spoiler_words.append(w)
-                is_spoiler = True
+    # for m in range(len(message)):
+    #     print(f"{m} : {message[m]}")
+
+    for i in spoiler_ranges:
+        start, end = i[0], i[1]
+
+        # spoiler range의 시작점 찾기
+        while True:
+            if message[start] == " " or start == 0:
                 break
+            start -= 1
 
-        if not is_spoiler:
-            no_spoiler_words.append(w)
+        while True:
+            if message[end] == " " or end == len(message) - 1:
+                if end == len(message) - 1:
+                    end += 1
+                break
+            end += 1
 
-    answer = set()
+        for w in message[start:end].strip().split():
+            spoiler_words.append(w)
+
+        if start != 0:
+            no_spoiler_ranges.append([idx, start + 1])
+        idx = end
+
+    if idx != len(message):
+        no_spoiler_ranges.append([idx, len(message)])
+    
+    for s, e in no_spoiler_ranges:
+        tmp = message[s:e].strip().split()
+        for t in tmp:
+            no_spoiler_words.append(t)
+
+    spoiler_words = set(spoiler_words)
     for sw in spoiler_words:
         if sw not in no_spoiler_words:
-            answer.add(sw)
-    return len(answer)
+            answer += 1
+            
+    return answer
