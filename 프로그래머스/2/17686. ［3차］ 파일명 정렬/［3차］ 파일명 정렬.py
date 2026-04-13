5aafeec266ba0cab3b682114a95de00c1f6e89f5
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-04-13 18:21:33 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.33 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,25 @@
+def solution(files):
+    f_lst = []
+    for f in files:
+        num_s, num_e = 0, 0
+        for j in range(len(f)):
+            if f[j].isdigit():
+                num_s = j
+                break
+
+        for j in range(num_s, len(f)):
+            if not f[j].isdigit():
+                num_e = j
+                break
+        else:
+            num_e = len(f)
+
+        head = f[:num_s].lower()
+        number = int(f[num_s:num_e])
+        tail = f[num_e:]
+
+        print(f" head : {head}, number : {number}, tail : {tail}")
+        f_lst.append([head, number, tail, f])
+    f_lst.sort(key=lambda x: (x[0], x[1]))
+
+    return [f[3] for f in f_lst]
