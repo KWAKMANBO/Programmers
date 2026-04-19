f713abc1b79f71af8e828daf87ad2d92ba68a3a8
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-04-20 01:04:16 +0900

    [Silver II] Title: 제곱수의 합, Time: 5168 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -3,15 +3,11 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+
 dp = [i for i in range(N + 1)]
 
 for i in range(1, N + 1):
-    if int(i ** 0.5) == i ** 0.5:
-        dp[i] = 1
-    else:
-        tmp = 1000000
-        for j in range(1,int(i**0.5) + 1):
-            tmp = min(tmp, dp[i-j**2])
-        dp[i] = tmp + 1
+    for j in range(1, int(i ** 0.5) + 1):
+        dp[i] = min(dp[i], dp[i - j * j] + 1)
 
 print(dp[N])
