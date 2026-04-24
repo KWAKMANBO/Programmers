d088ac3c5349a4691676cde469cfff57a36eacee
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-04-24 15:03:27 +0900

    [level 2] Title: 가장 큰 수, Time: 771.88 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,18 @@
+from functools import cmp_to_key
+
+def compare(a, b):
+    if a + b > b + a:
+        return -1
+    elif a + b < b + a:
+        return 1
+    return 0
+
+
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    answer = "".join(numbers)
-    return answer if int(answer) != 0 else '0'
+
+    numbers.sort(key=cmp_to_key(compare))
+
+    return "".join(numbers) if int("".join(numbers)) != 0 else "0"
+
+
