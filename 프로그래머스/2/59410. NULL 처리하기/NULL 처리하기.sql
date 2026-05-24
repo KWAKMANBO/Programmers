d9a9744318c6d3047f747bbb4f48452ff1c3f245
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-05-24 18:45:27 +0900

    [level 2] Title: NULL 처리하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -1,2 +1,3 @@
--- 코드를 입력하세요
-SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name') as NAME, SEX_UPON_INTAKE FROM ANIMAL_INS ORDER BY ANIMAL_ID;
+select animal_type, ifnull(name, 'No name') as name, sex_upon_intake
+from animal_ins
+order by animal_id 
