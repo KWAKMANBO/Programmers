11d51d01a52727b8498d4777622dc7483809025f
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-10 10:18:06 +0900

    [level 1] Title: 노란불 신호등, Time: 119.48 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -2,37 +2,40 @@ import math
 
 
 def solution(signals):
+    answer = 0
+    # 각 신호등의 주기를 저장할 배열
     periods = [0] * len(signals)
-
-    def is_yellow(t, g, y, r):
-        cycle = g + y + r
-        status = (t - 1) % cycle
-        return g <= status < g + y
-
+    # 신호등의 상태를 계산할 최소 시간 -> 이 이후로는 패턴이 반복됨
     MAX_SECOND = 1
-    for i in range(len(signals)):
-        periods[i] = sum(signals[i])
-        MAX_SECOND = (MAX_SECOND * periods[i]) // math.gcd(MAX_SECOND, periods[i])
 
-    yellow_board = []
+    # periods초기화 및 MAX_SECOND계산
+    for i in range(len(signals)):
+        # 주기를 계산
+        periods[i] = sum(signals[i])
+        # MAX_SECOND를 계산
+        MAX_SECOND = math.lcm(MAX_SECOND, periods[i])
+
+    yellows = []
+
     for i in range(len(signals)):
         tmp = [0] * periods[i]
-        g, y = signals[i][0], signals[i][1]
+        # 초록불과 노란불 사이를 1로 할당해 노란불임을 표시
+        for j in range(signals[i][0], signals[i][0] + signals[i][1]):
+            tmp[j] = 1
 
-        for i in range(g, g + y):
-            tmp[i] = 1
+        yellows.append(tmp)
 
-        yellow_board.append(tmp)
+    for t in range(1, MAX_SECOND):
 
-    for t in range(1, MAX_SECOND + 1):
-
-        all_yellow = True
-        for s in signals:
-            if not is_yellow(t, s[0], s[1], s[2]):
-                all_yellow = False
-
-        if all_yellow:
+        all_yellow_flag = True
+        for i in range(len(signals)):
+            idx = (t - 1) % periods[i]
+            if yellows[i][idx] != 1:
+                all_yellow_flag = False
+                break
+        if all_yellow_flag:
             return t
 
     return -1
 
+    return answer
