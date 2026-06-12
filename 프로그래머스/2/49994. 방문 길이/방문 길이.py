bb7e155f5193f4aa0dd65cd9246c9c25c4487b3b
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-12 13:20:43 +0900

    [level 2] Title: 방문 길이, Time: 0.13 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,15 +1,20 @@
 def solution(dirs):
-    answer = 0
-    pos = (0, 0)
-    move_lst = set()
-    dir_dict = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
-    for d in dirs:
-        dx, dy = dir_dict[d]
-        x, y = pos
-        nx, ny = x + dx, y + dy
-        if -5 <= nx <= 5 and -5 <= ny <= 5:
-            pos = (nx, ny)
-            move_lst.add((x, y, nx, ny))
-            move_lst.add((nx, ny, x, y))
+    direction = {'R': (1, 0), 'L': (-1, 0), 'U': (0, 1), 'D': (0, -1)}
 
-    return len(move_lst)//2
+    cur_pos = (0, 0)
+
+    visited = set()
+    for d in dirs:
+        cx, cy = cur_pos
+        dx, dy = direction[d]
+        nx, ny = cx + dx, cy + dy
+        # print(f"cur : {(cx, cy)}, next : {(nx, ny)}")
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            cur_pos = (nx, ny)
+            if cx > nx:
+                nx, cx = cx, nx
+            if cy > ny:
+                cy, ny = ny, cy
+            visited.add((cx, cy, nx, ny))
+
+    return len(visited)
