9b98130a5204f68ed77bfe53e20932ea4e464fc2
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-18 11:09:23 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 249.03 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -1,16 +1,29 @@
 def solution(numbers):
     answer = []
+    bin_numbers = []
+
     for n in numbers:
         if n % 2 == 0:
-            answer.append(n+1)
+            bin_numbers.append(bin(n)[2:])
+        if n % 2 == 1:
+            bin_numbers.append("0" + bin(n)[2:])
+
+    # 짝수
+    # - 짝수는 무조건 맨마지막이 비트 1의자리 비트가 0 이므로 그냥 +1 해서 0을 1로 만들어주면됨
+    # 홀수
+    # - 홀수는 왼쪽부터 확인하면서 맨처음으로 만나는 01을 10으로 바꿔 주면됨
+
+    for b in bin_numbers:
+        if b[-1] == '0':
+            tmp = b[:-1] + '1'
+            answer.append(int(tmp, 2))
         else:
-            bin_n = bin(n)[2:][::-1]
-            bin_n = bin_n + "0"
-            for i in range(len(bin_n)):
-                if bin_n[i:i+2] == "10":
-                    bin_n = bin_n[:i] + "01" + bin_n[i+2:]
+            tmp = b
+            for i in range(len(b) - 1, 0, -1):
+                if tmp[i - 1: i + 1] == "01":
+                    tmp = tmp[:i-1] + "10" + tmp[i + 1:]
+                    answer.append(int(tmp, 2))
                     break
-            answer.append(int(bin_n[::-1],2))
 
 
     return answer
