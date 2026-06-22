054cd04ad6dc5492fb9c66b779828d77d5ad83c0
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-22 10:04:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.42 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,24 +2,23 @@ from collections import deque
 
 
 def solution(maps):
-    answer = 0
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
+    N, M = len(maps), len(maps[0])
 
-    m = len(maps)
-    n = len(maps[0])
-    q = deque([[0, 0]])
+    q = deque()
+    q.append((0, 0, 1))
+    visited = set()
 
-    # BFS
     while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < m and 0 <= ny < n and maps[nx][ny] == 1:
-                if nx == 0 and ny == 0 :
-                    continue
-                q.append([nx, ny])
-                maps[nx][ny] = maps[x][y] + 1
+        x, y, c = q.popleft()
 
-    return maps[m-1][n-1] if maps[m-1][n-1] != 1 else -1
+        if (x,y) == (N-1,M-1):
+            return c
+
+        for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < N and 0 <= ny < M and maps[nx][ny] != 0 and (nx, ny) not in visited:
+                q.append((nx, ny, c + 1))
+                visited.add((nx, ny))
+
+    return -1
