0fda436d177d5d506f4a39098dff51994c1e5e47
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-23 12:09:56 +0900

    [level 2] Title: 디펜스 게임, Time: 185.92 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -3,16 +3,18 @@ import heapq
 
 def solution(n, k, enemy):
     answer = 0
-    q = []
-
+    hq = []
     for e in enemy:
-        heapq.heappush(q, -1 * e)
 
+        # 일단 적을 빼기
         n -= e
+        heapq.heappush(hq, -e)
 
+        # 적을 뺴고 난 후 내 병사에서 감당할 수 없다면
         if n < 0:
             if k > 0:
-                n += -1 * heapq.heappop(q)
+                # 무적권을 사용
+                n += -1*heapq.heappop(hq)
                 k -= 1
             else:
                 break
