72989c491c4c2af493053be94c9b6e09c2936d40
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-28 03:04:03 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT AI.ANIMAL_ID, AI.NAME
-FROM ANIMAL_INS AS AI JOIN ANIMAL_OUTS AS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-ORDER BY  AO.DATETIME - AI.DATETIME DESC
-LIMIT 2;
+select ai.animal_id, ai.name
+from animal_ins as ai join animal_outs as ao on ai.animal_id = ao.animal_id
+order by abs(datediff(ao.datetime, ai.datetime)) desc
+limit 2
