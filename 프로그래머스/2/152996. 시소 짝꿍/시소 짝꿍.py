4a835650e75fe6aa52d79669e09c0022b8ce19e5
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-06-29 14:31:58 +0900

    [level 2] Title: 시소 짝꿍, Time: 4.04 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -3,19 +3,18 @@ from collections import Counter
 
 def solution(weights):
     answer = 0
-    c = Counter(weights)
-    # 1:1 비율 구하기
-    for k, v in c.items():
-        if v > 1:
-            answer += (v * (v - 1)) // 2
+    counter = Counter(weights)
 
-    # 나머지 비율 구하기
-    for w in set(weights):
-        if (2 / 3 * w) in weights:
-            answer += c[w] * c[2 / 3 * w]
-        if (1 / 2 * w) in weights:
-            answer += c[w] * c[(1 / 2 * w)]
-        if (3 / 4 * w) in weights:
-            answer += c[w] * c[(3 / 4 * w)]
+    # 1:1 비율 부터 계산
+    for k, v in counter.items():
+        if v > 1:
+            answer += v * (v - 1) // 2
+
+        if (2/3)*k in counter:
+            answer += v * counter[(2/3)*k]
+        if (1/2)*k in counter:
+            answer += v * counter[(1/2)*k]
+        if (3/4)*k in counter:
+            answer += v * counter[(3/4)*k]
 
     return answer
