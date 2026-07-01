bbece3413850925fc3d15ca007752234e96c4ce5
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-07-01 16:41:04 +0900

    [level 2] Title: 택배상자, Time: 122.20 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.py
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -1,14 +1,30 @@
+from collections import deque
+
+
 def solution(order):
-    stack = []
-    N = len(order)
-    i = 1
+    answer = 0
+    tmp_belt = deque([])
+    belt = deque([i for i in range(1, len(order) + 1)])
     idx = 0
-    while i < N + 1:
-        stack.append(i)
-        while stack[-1] == order[idx]:
+    # order의 idx    idx = 0  
+    while True:
+        # 컨테이너 벨트가 비어 있지 않고, 현재 order의 순서와 같은 상자라면  
+        if belt and belt[0] == order[idx]:
+            belt.popleft()
+            answer += 1
             idx += 1
-            stack.pop()
-            if len(stack) == 0:
-                break
-        i += 1
-    return idx
+            # 임시 벨트의 가장 마지막 원소가 order의 순서와 같은 상자라면  
+        elif tmp_belt and tmp_belt[-1] == order[idx]:
+            tmp_belt.pop()
+            answer += 1
+            idx += 1
+            # 현재 벨트의 맨앞 원소가 order와 다르다면 임시에 추가  
+        elif belt and belt[0] != order[idx]:
+            tmp_belt.append(belt.popleft())
+            # belt와 tmp_belt가 둘다 비었따면  
+        elif not belt and not tmp_belt:
+            break
+        else:
+            break
+
+    return answer
