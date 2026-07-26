ccf396dce49e65f68bcb9c89dcaf291021b6a371
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-07-26 17:18:20 +0900

    [level 2] Title: N-Queen, Time: 3495.87 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,47 +1,36 @@
 def solution(n):
     answer = 0
 
-    board = [[0] * n for _ in range(n)]
+    # pos[i] -> i는 row(행), pos[i]는 col(열)
+    pos = [0] * n
 
-    def check(r, c):
-        # col확인
-        i = r - 1
-        while i >= 0:
-            if board[i][c] == 1:
+    def check(row):
+        # 0부터 row - 1까지 이전에 넣은 퀸들이 충돌하는게 있는지 검사
+        for i in range(row):
+            # i는 행을 pos[i]는 열을 의미하므로
+            # pos[i]는 i번째 행의 pos[i]의 열에 퀸이 있음을 의미
+            # pos[i] == pos[row]는 같은 열에 퀸이 있음을 의미하므로 False를 반환
+            if pos[i] == pos[row]:
                 return False
-            i -=1
 
-        # 왼쪽 대각선 위 확인
-        i = r - 1
-        j = c - 1
-        while i >= 0 and j >= 0:
-            if board[i][j] == 1:
+            # 대각선 확인
+            # row와 i의 행의 차이와, 열의 차이가 같다면 즉 대각서에 있음을 의미함
+            if abs(pos[i] - pos[row]) == abs(row - i):
                 return False
-            i -= 1
-            j -= 1
-
-        # 오른쪽 대각선 위 확인
-        i = r - 1
-        j = c + 1
-        while i >= 0 and j < n:
-            if board[i][j] == 1:
-                return False
-            i -= 1
-            j += 1
 
         return True
 
-    def dfs(r):
+    def dfs(row):
         nonlocal answer
-        if r == n:
+        if row == n:
             answer += 1
             return
 
-        for j in range(n):
-            if check(r, j):
-                board[r][j] = 1
-                dfs(r + 1)
-                board[r][j] = 0
+        for i in range(n):
+            pos[row] = i
+            if check(row):
+                dfs(row + 1)
 
     dfs(0)
+
     return answer
