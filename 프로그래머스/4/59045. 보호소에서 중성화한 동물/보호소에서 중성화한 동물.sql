d1406a196bed254b6a05c28253461d7465582b53
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-07-31 12:00:07 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,5 +1,9 @@
--- 코드를 입력하세요
-SELECT AI.ANIMAL_ID, AI.ANIMAL_TYPE, AI.NAME
-FROM ANIMAL_INS AS AI JOIN ANIMAL_OUTS AS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE AI.SEX_UPON_INTAKE LIKE 'Intact%' AND AO.SEX_UPON_OUTCOME NOT LIKE 'Intact%' 
-ORDER BY AI.ANIMAL_ID 
+select  ai.animal_id, ai.animal_type, ai.name
+from animal_ins ai join animal_outs ao on ai.animal_id =ao.animal_id
+where 
+    ai.sex_upon_intake like 'Intact%' 
+    and 
+    (ao.sex_upon_outcome like 'Spayed%' 
+    or
+     ao.sex_upon_outcome like 'Neutered%'
+    )
