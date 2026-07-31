cae8f6be42802a46c4e99dfa6b5b709ddd018752
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-07-31 11:43:51 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT AI.ANIMAL_ID, AI.NAME
-FROM ANIMAL_INS AS AI JOIN ANIMAL_OUTS AS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE AI.DATETIME > AO.DATETIME
-ORDER BY AI.DATETIME 
+select ai.animal_id, ai.name
+from animal_ins ai join animal_outs ao on ai.animal_id = ao.animal_id
+where ao.datetime < ai.datetime
+order by ai.datetime asc
