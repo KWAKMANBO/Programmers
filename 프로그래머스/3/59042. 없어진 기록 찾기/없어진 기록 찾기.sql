79f8930987417ccad1f5946228c1bed1d7619ac5
KWAKMANBO <113917771+KWAKMANBO@users.noreply.github.com>
2026-08-03 03:08:07 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
-select ao.animal_id as ANIMAL_ID, ao.name as NAME
-from animal_ins as ai right join animal_outs as ao on ai.animal_id = ao.animal_id
+select ao.animal_id, ao.name
+from animal_ins ai right join animal_outs ao on ai.animal_id = ao.animal_id
 where ai.animal_id is null
+order by animal_id
